9d1af4005e2c58e58cbd2c55511af9a2b3c07bbc
robinlacruz <robin.lacruz@gmail.com>
2022-04-16 12:51:54 -0300

    cardWithTrailProgressBar

--- elearningProject/force-app/main/default/lwc/trailView/trailView.js
+++ elearningProject/force-app/main/default/lwc/trailView/trailView.js
@@ -4,6 +4,9 @@ import DESCRIPTION_FIELD from '@salesforce/schema/Trail__c.Description__c';
 import TRAILTIME_FIELD from '@salesforce/schema/Trail__c.Trail_Estimated_Time__c';
 import TRAILSCORE_FIELD from '@salesforce/schema/Trail__c.Trail_Max_Score__c'; */
 import getTrail from '@salesforce/apex/TrailController.getTrail';
+import getProgressTrail from '@salesforce/apex/TrailController.getProgressTrail';
+import getPassedModuleIds from '@salesforce/apex/TrailController.getPassedModuleIds';
+import getModules from '@salesforce/apex/TrailController.getModules';
 
 /* const FIELDS = [
     'Trail__c.Name',
@@ -18,6 +21,12 @@ export default class TrailView extends LightningElement {
 
   @wire(getTrail, {trailId : '$recordId'/* , fields : FIELDS */})
   trail;
+  @wire(getProgressTrail, {trailId : '$recordId'})
+  progressTrail;
+  @wire(getPassedModuleIds, {trailId : '$recordId'})
+  passedModuleIds;
+  @wire(getModules, {trailId: '$recordId'})
+  Modules; 
 
 get name() {
     //console.log('hay console.log: ',this.trail.data);
@@ -38,5 +47,31 @@ get trailScore() {
 
 get fulltrail() {
         return this.trail.data;
+    } 
+
+get progressIsFalse() {
+    if(this.progressTrail.data==0){
+        return true;
+        } else if(this.progressTrail.data==100){
+        return false;
+        }
     }
+
+get progressTrailPerc() {
+    if(this.progressTrail.data>0 && this.progressTrail.data<100)
+        return this.progressTrail.data;
+    }
+
+get numberOfPassedModules() {
+        return this.passedModuleIds.data?.length||0;
+    }
+
+get NumberOfModules() {
+        return this.Modules.data?.length||0;
+    }
+
+/* get showTrailWrap() {
+    console.log('el trailWrap trae: ',JSON.stringify(this.trailWrap?.data) || 'no hay nada en data');
+    return JSON.stringify(this.trailWrapList[0]);
+    } */
 }

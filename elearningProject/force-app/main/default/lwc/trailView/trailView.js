63b86a37fff4d6300868a42e89edc151f694e6a8
robinlacruz <robin.lacruz@gmail.com>
2022-04-21 15:35:33 -0300

    trailCompletedYModuleCompletedConChecksYBarraProg

--- elearningProject/force-app/main/default/lwc/trailView/trailView.js
+++ elearningProject/force-app/main/default/lwc/trailView/trailView.js
@@ -2,7 +2,6 @@ import { LightningElement, api, wire } from 'lwc';
 
 import getTrailWrap from '@salesforce/apex/TrailController.getTrailWrap';
 
-
 export default class TrailView extends LightningElement {
     
   @api recordId;
@@ -12,27 +11,22 @@ export default class TrailView extends LightningElement {
   trailMaxScore;
   trailProgress;
   trailCompleted;
-
+  modulesWithCheck;
   
   @wire(getTrailWrap, {trailId: '$recordId'})
   showTrailWrap({data, error}){
+     // debugger;
     if(data){
         this.trailName = data.trail.Name;
         this.trailDescription = data.trail.Description__c;
         this.trailEstimatedTime = data.trail.Trail_Estimated_Time__c;
         this.trailMaxScore = data.trail.Trail_Max_Score__c;
         if(data.progressTrail) this.trailProgress = data.progressTrail;
-        if(data.progressTrail==100) this.trailCompleted = true;
-    }else if(error){
-        console.log('HUBO UN ERROR------>',error);
-    }
-  }
+        this.trailCompleted = data.progressTrail==100? true : false;
+        if(data.modulesWithCheck) this.modulesWithCheck=data.modulesWithCheck;        
 
-  get trailNotStarted(){
-    if(this.trailProgress==0){
-        return true;
-    } else {
-        return false;
+    }else if(error){
+        console.log('HUBO UN ERROR------>',error);  
     }
   }
-}
+} 
